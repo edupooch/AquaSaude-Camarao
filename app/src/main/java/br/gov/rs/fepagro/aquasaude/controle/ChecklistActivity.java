582fd773bd85fe13b9f90eaa25aaa2649a696fec
edupooch <edupooch@gmail.com>
2016-10-05 12:16:08 -0300

    - Tela boas práticas / Checklist

---
+++ app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/ChecklistActivity.java
@@ -0,0 +1,30 @@
+package br.gov.rs.fepagro.aquasaude.controle;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import br.gov.rs.fepagro.aquasaude.R;
+
+public class ChecklistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
