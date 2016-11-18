3bb9fc3e927b22e10adfb28188533c9990788392
edupooch <edupooch@gmail.com>
2016-11-18 12:35:02 -0200

    - Correção Ortográfica

--- app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/ChecklistActivity.java
+++ app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/ChecklistActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
 import br.gov.rs.fepagro.aquasaude.R;
 
 public class ChecklistActivity extends AppCompatActivity {
@@ -16,6 +18,14 @@ public class ChecklistActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checklist);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        View btConfirma = findViewById(R.id.bt_confirma_checklist);
+        assert btConfirma != null;
+        btConfirma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChecklistActivity.this, "TODO: realizar análise das respostas", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
