71be45db6443ab1264473b239da5d84d15c4f016
Eduardo H P Pooch <edupooch@gmail.com>
2017-02-23 20:08:54 -0300

    resultados do checklist

--- app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/biosseguranca/ChecklistFragment.java
+++ app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/biosseguranca/ChecklistFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.gov.rs.fepagro.aquasaude.R;
 
@@ -16,19 +20,42 @@ import br.gov.rs.fepagro.aquasaude.R;
  */
 public class ChecklistFragment extends Fragment {
 
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.content_checklist, container, false);
+        view = inflater.inflate(R.layout.content_checklist, container, false);
 
-        View btConfirma =  view.findViewById(R.id.bt_confirma_checklist);
+        View btConfirma = view.findViewById(R.id.bt_confirma_checklist);
         assert btConfirma != null;
-        btConfirma.setOnClickListener(v ->
-                getFragmentManager().beginTransaction()
-                .replace(R.id.content_frame, new BoasPraticasFragment())
-                .commit());
-
+        btConfirma.setOnClickListener(v -> {
+            int[] desmarcados = getItensDesmarcados();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, ResultadoChecklistFragment.newInstance(desmarcados))
+                    .commit();
+        });
         return view;
+    }
+
+    private int[] getItensDesmarcados() {
+        int[] checkBoxResId = new int[]{R.id.checkbox_1, R.id.checkbox_2, R.id.checkbox_3,
+                R.id.checkbox_4, R.id.checkbox_5, R.id.checkbox_6, R.id.checkbox_7, R.id.checkbox_8,
+                R.id.checkbox_9, R.id.checkbox_10};
+
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        for (int i = 0; i < checkBoxResId.length; i++) {
+            int resId = checkBoxResId[i];
+            CheckBox check = (CheckBox) view.findViewById(resId);
+            if (!check.isChecked()) {
+                lista.add(i);
+            }
+        }
+
+        int[] array = new int[lista.size()];
+        for (int i = 0; i < lista.size(); i++) array[i] = lista.get(i);
+        return array;
     }
 
 
--- app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/biosseguranca/ResultadoChecklistFragment.java
+++ app/src/main/java/br/gov/rs/fepagro/aquasaude/controle/biosseguranca/ResultadoChecklistFragment.java
@@ -1,12 +1,15 @@
 package br.gov.rs.fepagro.aquasaude.controle.biosseguranca;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import br.gov.rs.fepagro.aquasaude.R;
 
@@ -15,12 +18,12 @@ import br.gov.rs.fepagro.aquasaude.R;
  * Use the {@link ResultadoChecklistFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ResultadoChecklistFragment extends Fragment {
+public class ResultadoChecklistFragment extends android.app.Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_ITENS_DESMARCADOS = "desmarcados";
 
     private int[] itens;
-
+    private View view;
 
 
     public ResultadoChecklistFragment() {
@@ -34,7 +37,7 @@ public class ResultadoChecklistFragment extends Fragment {
      * @param itensDesmarcados itens que não foram marcados na checklist.
      * @return A new instance of fragment ResultadoChecklistFragment.
      */
-    public static ResultadoChecklistFragment newInstance(int[] itensDesmarcados) {
+    public static android.app.Fragment newInstance(int[] itensDesmarcados) {
         ResultadoChecklistFragment fragment = new ResultadoChecklistFragment();
         Bundle args = new Bundle();
         args.putIntArray(ARG_ITENS_DESMARCADOS, itensDesmarcados);
@@ -46,22 +49,37 @@ public class ResultadoChecklistFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            itens = getArguments().getIntArray(ARG_ITENS_DESMARCADOS);
-        }
+        itens = getArguments().getIntArray(ARG_ITENS_DESMARCADOS);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_resultado_checklist, container, false);
+        view = inflater.inflate(R.layout.fragment_resultado_checklist, container, false);
 
+        int[] stringsResId = new int[]{R.string.resposta_1, R.string.resposta_2, R.string.resposta_3,
+                R.string.resposta_4, R.string.resposta_5, R.string.resposta_6, R.string.resposta_7,
+                R.string.resposta_8, R.string.resposta_9, R.string.resposta_10};
 
+        if (itens == null || itens.length == 0) {
+            view.findViewById(R.id.dicas_titulo).setVisibility(View.GONE);
+
+        } else {
+            StringBuilder builderDicas = new StringBuilder();
+            for (int indice : itens) {
+                String dica = getString(stringsResId[indice]);
+                builderDicas.append(dica).append("\n");
+            }
+            String dicas = builderDicas.toString();
+            TextView textViewDicas = (TextView) view.findViewById(R.id.dicas);
+            textViewDicas.setText(dicas);
+        }
+
+        Button btOk = (Button) view.findViewById(R.id.bt_ok);
+        btOk.setOnClickListener(v -> getActivity().finish());
         return view;
     }
-
-
 
 
 }
